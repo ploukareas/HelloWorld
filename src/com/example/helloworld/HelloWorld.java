e3c193b8d38d4833705e34f81185c4e64c1227f5
ploukareas <ploukareas@gmail.com>
2022-06-19 17:15:51 +0300

    new project structure

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.example.helloworld;
+
+import java.util.Arrays;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld.main");
+        System.out.println("args = " + Arrays.deepToString(args));
+        System.out.printf("Hellowordl");
+    }
+}
